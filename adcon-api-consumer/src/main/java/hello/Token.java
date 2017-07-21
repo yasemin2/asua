a64865491ac420fe209770a867476d6a52859fd4
Alex Vulpe <alex.vulpe@radio.pub.ro>
2017-07-21 16:40:27 +0300

    Simple application that does the following
    calls Adcon API and retrieves acces token
    
    uses access token to make authorized calls and return Adcon data

---
+++ adcon-api-consumer/src/main/java/hello/Token.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Token {
+	
+	private Result result;
+
+	public Result getResult() {
+		return result;
+	}
+
+	public void setResult(Result result) {
+		this.result = result;
+	} 
+
+}
